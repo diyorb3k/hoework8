0c2fc6ea18a10082a9ac2c604e21204a6cbf2a58
unknown <you@example.com>
2024-05-06 22:47:39 +0600

    ehrrefeutuieguefdjvdkfg

---
+++ main.js
@@ -0,0 +1,69 @@
+//  1-CHI SITRING
+// const text = prompt("biror bir  harf kiriting...");
+
+// function printLargeLetters(n) {
+//   if (n < 1 || n > 26) {
+//     console.log("Narxi noto'g'ri! N >= 1 va N <= 26 bo'lishi kerak.");
+//     return;
+//   }
+// }
+
+// printLargeLetters(text);
+
+// for (let i = 0; i < n; i++) {
+//   let letter = String.fromCharCode(65 + i);
+//   let line = letter.repeat(i + 1);
+//   console.log(line);
+// }
+// let n = parseInt(prompt("Narxni kiriting (1 <= n <= 26): "));
+// printLargeLetters(n);
+// 5-CHI SITRING
+// let s = prompt("");
+// let reversedString = s.split("").reverse().join("");
+// console.log(reversedString);
+
+// 3 chistring
+// let a = "salom";
+// console.log(a[0], a[0].charCodeAt());
+// console.log(a[a.length - 1], a[a.length - 1].charCodeAt());
+// 26-Dars
+// let str = "hillo  world!";
+// let nevStr = str.replace([0], "H"[str.length - 1]("W"));
+// console.log(nevStr);
+// 12 - string;
+// let s = prompt();
+// let reversedString = s.split("").reverse().join("");
+// console.log(reversedString);
+// 9-string
+// let str = prompt("");
+// let newStr = str.toLowerCase();
+// console.log(newStr);
+// 10-string
+// let str = prompt("");
+// let newStr = str.toLowerCase();
+// let newStr1 = str.toUpperCase();
+// console.log(newStr);
+// console.log(newStr1);
+// 7-chistring
+
+// function countNumbers(string) {
+//   let count = 0;
+//   for (let char of string) {
+//     if (!isNaN(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// let testString = "Hello 123 World";
+// console.log(countNumbers(testString));
+// 4-chi String
+// function qil(N, alti) {
+//   let outpoot = alti.repeat(N);
+//   return outpoot;
+// }
+// let N = prompt("");
+// let alti = "A";
+// let outpoot = qil(N, alti);
+// console.log(outpoot);
